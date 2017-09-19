f6216d3ddfcdc52121022ad02206dfe5441659ce
antmedina <ant.n.medina@gmail.com>
2017-09-19 16:50:45 -0400

    second exercise compelted

---
+++ fulfill-a-promise.js
@@ -0,0 +1,20 @@
+//my initial attempt
+// //set strict mode
+// "use strict";
+// //establish my intial Promise
+// var promise = new Promise (function (fulfill, reject){
+//     return setTimeout(function() {return fulfill("FULFILLED!")}, 300)
+// }).then(console.log);
+
+//Better way below//
+//set strict mode
+'use strict';
+//create promise with fulfill
+ var promise = new Promise(function (fulfill, reject) {
+// create setTimeout with phrase
+      setTimeout(function () {
+        fulfill('FULFILLED!');
+      }, 300);
+    });
+//above code will execute and then set your console log with .then
+    promise.then(console.log)
