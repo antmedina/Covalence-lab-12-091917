5f0335d4c8a760f2b1a2ad6a1d2874be68c4e229
antmedina <ant.n.medina@gmail.com>
2017-09-19 11:12:38 -0400

    exercise 1 complete

---
+++ warm-up.js
@@ -0,0 +1,17 @@
+// this is the barebones code that should be accepted//
+setTimeout(function() { return console.log('TIMED OUT!');}, 300);
+
+//if not try this
+// (function() {setTimeout(function() { return console.log('TIMED OUT!');}, 300);}).call(this);//
+
+//or begin the your code with -
+//"use strict";
+//then 
+//setTimeout(function() { return console.log('TIMED OUT!');}, 300);//
+
+//Strict Mode is a new feature in ECMAScript 5 that allows you to place a program, or a function, in a "strict" operating context. This strict context prevents certain actions from being taken and throws more exceptions.
+// And: Strict mode helps out in a couple ways:
+// It catches some common coding bloopers, throwing exceptions.
+// It prevents, or throws errors, when relatively "unsafe" actions are taken (such as gaining access to the global object).
+// It disables features that are confusing or poorly thought out.
+
